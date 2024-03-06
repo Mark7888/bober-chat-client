abb3514d61d8a4147aa75b508f461173d84473f6
Mark7888 <l.mark7888@gmail.com>
2024-03-06 14:46:13 +0100

    cleared ConnectionHandler

--- app/src/main/java/me/mark7888/boberchat/ConnectionHandler.kt
+++ app/src/main/java/me/mark7888/boberchat/ConnectionHandler.kt
@@ -10,44 +10,5 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class ConnectionHandler(private val context: Context) {
-    fun sendAuthRequest(idToken: String?, email: String?, clientId: String?) {
-        val serverUri = context.getString(R.string.server_url) // replace with your server URI
-        val urlString = "$serverUri/auth?token=$idToken&email=$email&client_id=$clientId"
-
-        AsyncTask.execute {
-            try {
-                val url = URL(urlString)
-                val conn = url.openConnection() as HttpURLConnection
-                conn.requestMethod = "POST"
-                conn.setRequestProperty("Content-Type", "application/json; utf-8")
-                conn.setRequestProperty("Accept", "application/json")
-                conn.doOutput = true
-
-                val jsonInputString = """{
-                    "token": "$idToken",
-                    "email": "$email",
-                    "client_id": "$clientId"
-                }"""
-
-                OutputStreamWriter(conn.outputStream).use { writer ->
-                    writer.write(jsonInputString)
-                    writer.flush()
-                }
-
-                conn.inputStream.bufferedReader().use { reader ->
-                    val response = StringBuilder()
-                    var line = reader.readLine()
-                    while (line != null) {
-                        response.append(line)
-                        line = reader.readLine()
-                    }
-                    // Here you can handle the server's response
-                    println(response.toString())
-                }
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
+    
 }
