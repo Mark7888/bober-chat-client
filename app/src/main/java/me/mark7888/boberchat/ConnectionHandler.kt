7551cf4e9c2f14ac6e49f49c6d3490e75396fe45
Mark7888 <l.mark7888@gmail.com>
2024-03-10 21:34:21 +0100

    fixed post json function

--- app/src/main/java/me/mark7888/boberchat/ConnectionHandler.kt
+++ app/src/main/java/me/mark7888/boberchat/ConnectionHandler.kt
@@ -5,6 +5,7 @@ import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.awaitResponse
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
 import kotlinx.coroutines.runBlocking
 
@@ -12,19 +13,32 @@ import kotlinx.coroutines.runBlocking
 object ConnectionHandler {
     private const val SERVER_BASE_URL : String = "https://aef2-185-45-199-174.ngrok-free.app"
 
-    fun postRequestJson(endpoint: String, json: String): Int {
+    fun postRequestJson(endpoint: String, json: String) {
+        Fuel.post(SERVER_BASE_URL + endpoint)
+            .jsonBody(json)
+            .also { println(it) }
+            .response { _, _, _ -> }
+    }
+
+    fun postRequestJsonHold(endpoint: String, json: String): Int {
         var statusCode = 0
-        // hold the request until the response is received
-        runBlocking {
-            Fuel.post(SERVER_BASE_URL + endpoint)
-                .jsonBody(json)
-                .also { println(it) }
-                .response { _, response, _ ->
-                    statusCode = response.statusCode
-                }
+
+        val (request, response, result) = (SERVER_BASE_URL + endpoint)
+            .httpPost().jsonBody(json).responseString()
+
+        when (result) {
+            is Result.Failure -> {
+                val ex = result.getException()
+                Log.e("ConnectionHandler", ex.toString())
+            }
+            is Result.Success -> {
+                statusCode = response.statusCode
+            }
         }
+
         return statusCode
     }
+
 
     fun getRequestJson(endpoint: String): String {
         var responseJson = "[]"
