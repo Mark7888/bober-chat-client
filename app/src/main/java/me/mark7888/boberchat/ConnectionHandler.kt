83b4622ad1dc07d83b089f62a4b587dd4080dc28
Mark7888 <l.mark7888@gmail.com>
2024-03-10 16:18:09 +0100

    fixed const base_url variable and returning status code on http request

--- app/src/main/java/me/mark7888/boberchat/ConnectionHandler.kt
+++ app/src/main/java/me/mark7888/boberchat/ConnectionHandler.kt
@@ -1,21 +1,21 @@
 package me.mark7888.boberchat
 
-import android.content.Context
-import android.content.res.Resources
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.extensions.jsonBody
-import java.net.HttpURLConnection
-import java.net.URL
-import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.result.Result
+import kotlinx.coroutines.runBlocking
 
 
 object ConnectionHandler {
-    private val serverUrl : String = "https://e49f-185-45-199-174.ngrok-free.app"
-    fun postRequestJson(endpoint: String, json: String) {
-        Fuel.post(serverUrl + endpoint)
+    private const val SERVER_BASE_URL : String = "https://aef2-185-45-199-174.ngrok-free.app"
+
+    fun postRequestJson(endpoint: String, json: String): Int {
+        var statusCode = 0
+        Fuel.post(SERVER_BASE_URL + endpoint)
             .jsonBody(json)
             .also { println(it) }
-            .response { result -> println(result)}
+            .response { _, response, _ ->
+                statusCode = response.statusCode
+            }
+        return statusCode
     }
 }
